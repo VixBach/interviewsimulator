dec16e5b38c8f5ae438bfa796de9d02745c6c92d
Vix Bach <vix.b@itcgroup.io>
2023-08-30 14:07:36 +0700

    update MD5 caching

--- application.72041.js
+++ application.72041.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.json';
+          this.settingsPath = 'src/settings.5c48a.json';
           this.showFPS = false;
         }
 
